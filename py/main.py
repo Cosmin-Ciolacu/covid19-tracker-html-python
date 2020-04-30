23e1cb7db84dcf573006e3219e8b231610ec0d6e
Cosmin Ciolacu <cosmin.ciolacu@dev51.io>
2020-04-30 15:03:00 +0300

    aplicatie finalizata

--- py/main.py
+++ py/main.py
@@ -1,36 +1,42 @@
-from browser import document, console
-from browser import ajax
+from browser import document, console, alert, ajax
 
 url = 'https://coronavirus-19-api.herokuapp.com/countries/'
 
 def get_response(req):
-  document['info'].style.display = 'flex'
-  console.log('se cauta date')
-  import json
-  data = json.loads(req.responseText)
-  console.log(data)
-  cazuri_totale = data['cases']
-  vindecati = data['recovered']
-  decedati = data['deaths']
-  cazuri_24h = data['todayCases']
-  decedati_24h = data['todayDeaths']
-  document['cases'].text = cazuri_totale
-  document['vindecati'].text = vindecati
-  document['decedati'].text = decedati
-  document['cases24'].text = cazuri_24h
-  document['decedati24'].text = decedati_24h
+  if req.responseText == 'Country not found':
+    alert('Nu se pot gasi date')
+  else:
+    document['info'].style.display = 'flex'
+    console.log('se cauta date')
+    import json
+    data = json.loads(req.responseText)
+    console.log(data)
+    cazuri_totale = data['cases']
+    vindecati = data['recovered']
+    decedati = data['deaths']
+    cazuri_24h = data['todayCases']
+    decedati_24h = data['todayDeaths']
+    document['cases'].text = cazuri_totale
+    document['vindecati'].text = vindecati
+    document['decedati'].text = decedati
+    document['cases24'].text = cazuri_24h
+    document['decedati24'].text = decedati_24h
 
 
 
 def cautare(e):
+  document['info'].style.display = 'none'
   country = document['country'].value
-  #console.log(country)
-  full_url = url + country 
-  #console.log(full_url)
-  req = ajax.ajax()
-  req.open('GET', full_url, True)
-  req.bind('complete', get_response)
-  req.send()
+  if len(country) != 0:
+    #console.log(country)
+    full_url = url + country 
+    #console.log(full_url)
+    req = ajax.ajax()
+    req.open('GET', full_url, True)
+    req.bind('complete', get_response)
+    req.send()
+  else:
+    alert('completati acest camp pentru a afisa datele')
 
 
 document['cauta'].bind('click', cautare)
